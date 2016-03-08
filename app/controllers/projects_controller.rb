05595022ff5ac91ae0c1902e1a2dca5562d04363
Shawn Kearney <shawnpk@ineptsoftware.com>
2016-03-07 19:44:35 -0800

    Add friendly_id for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -31,7 +31,7 @@ class ProjectsController < ApplicationController
 
   private
     def find_project
-      @project = Project.find(params[:id])
+      @project = Project.friendly.find(params[:id])
     end
     def project_params
       params.require(:project).permit(:title, :description, :link)
