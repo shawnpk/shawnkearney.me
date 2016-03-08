463b85bf8239a4c522e7fae0d12722a922f9c9af
Shawn Kearney <shawnpk@ineptsoftware.com>
2016-03-07 19:50:33 -0800

    Add edit and destroy links to projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -23,10 +23,15 @@ class ProjectsController < ApplicationController
     end
   end
   def update
-
+    if @project.update(project_params)
+      redirect_to @project, notice: 'Nice Shawn! That project was successfully updated.'
+    else
+      render :edit
+    end
   end
   def destroy
-
+    @project.destroy
+    redirect_to projects_path
   end
 
   private
