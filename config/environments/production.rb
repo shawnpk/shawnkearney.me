e82209b130d74c143ed51edcca5036451ed39b42
Shawn Kearney <shawnpk@ineptsoftware.com>
2016-03-07 22:17:19 -0800

    Update production.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url.options = { host: 'shawnkearney.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://shawnkearney.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
