18cbcc83e94fe24e8b6b9c0254c0f766eef266c8
Shawn Kearney <shawnpk@ineptsoftware.com>
2016-03-07 18:48:16 -0800

    Update posts_controller file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,7 +36,7 @@ class PostsController < ApplicationController
 
   private
     def post_params
-      params.require(:post).permit(:title, :content, :slug)
+      params.require(:post).permit(:title, :content)
     end
     def find_post
       @post = Post.friendly.find(params[:id])
